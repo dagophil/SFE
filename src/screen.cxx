091cfb6ef346d8237937b74b26a20091d4112525
Philip Schill <philip.schill@gmx.de>
2016-04-18 21:08:32 +0200

    Implemented screen methods.

--- src/screen.cxx
+++ src/screen.cxx
@@ -1,1 +1,37 @@
 #include "SFE/screen.hxx"
+
+namespace sfe
+{
+
+    Screen::Screen(unsigned int window_width, unsigned int window_height)
+    {
+        resize(window_width, window_height);
+    }
+
+    void Screen::resize(unsigned int window_width, unsigned int window_height)
+    {
+        Widget::viewport_ratio = window_width / static_cast<float>(window_height);
+    }
+
+    void Screen::update(sf::Time elapsed_time)
+    {
+        gui_.update(elapsed_time);
+    }
+
+    void Screen::render(sf::RenderTarget & target) const
+    {
+        target.setView({ { 0.5f, 0.5f },{ 1.0f, 1.0f } });
+        gui_.render(target);
+    }
+
+    Widget & Screen::get_gui()
+    {
+        return gui_;
+    }
+
+    Widget const & Screen::get_gui() const
+    {
+        return gui_;
+    }
+
+} // namespace sfe
--- test/sfetests.cxx
+++ test/sfetests.cxx
@@ -2,43 +2,60 @@
 
 #include <SFML/Graphics.hpp>
 
+#include <SFE/screen.hxx>
 #include <SFE/widget.hxx>
 
-int main()
+void create_widgets(sfe::Widget & container)
 {
     using namespace sfe;
 
-    // Create some widgets.
-    ColorWidget container(sf::Color(255, 0, 0));
-    container.set_width(0.25);
-    container.set_height(0.25);
-    container.set_x(0.25);
-    container.set_y(0.25);
-    
+    // Create the red background.
+    auto background = std::make_unique<ColorWidget>(sf::Color(255, 0, 0));
+    background->set_width(0.25);
+    background->set_height(0.25);
+    background->set_x(0.25);
+    background->set_y(0.25);
+
+    // Add blue in the upper left corner.
     auto w0 = std::make_unique<ColorWidget>(sf::Color(0, 0, 255));
     w0->set_width(0.5);
     w0->set_height(0.5);
-    container.add_widget(std::move(w0));
 
-    auto w2 = std::make_unique<ImageWidget>("disk.png");
-    w2->set_scale(Scale::X);
-    w2->set_align_x(AlignX::Center);
-
+    // Add green in the lower right corner.
     auto w1 = std::make_unique<ColorWidget>(sf::Color(0, 255, 0));
     w1->set_width(0.5);
     w1->set_height(0.5);
     w1->set_x(0.5);
     w1->set_y(0.5);
+
+    // Add the disk image on top of the green.
+    auto w2 = std::make_unique<ImageWidget>("disk.png");
+    w2->set_scale(Scale::X);
+    w2->set_align_x(AlignX::Center);
     w1->add_widget(std::move(w2));
-    container.add_widget(std::move(w1));
+
+    // Nest the widgets.
+    background->add_widget(std::move(w0));
+    background->add_widget(std::move(w1));
+    container.add_widget(std::move(background));
+}
+
+int main()
+{
+    using namespace sfe;
+
+    unsigned int const WIDTH = 800;
+    unsigned int const HEIGHT = 600;
+
+    // Create the screen with some widgets.
+    Screen screen(WIDTH, HEIGHT);
+    create_widgets(screen.get_gui());
 
     // Create a clock to measure the elapsed time per frame.
     sf::Clock clock;
 
     // Create the window.
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Test");
-    Widget::viewport_ratio = 1.333333f; // 800 / 600
-    window.setView({ { 0.5f, 0.5f }, { 1.0f, 1.0f } });
+    sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Test");
 
     // Run the main loop.
     while (window.isOpen())
@@ -50,16 +67,16 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
             else if (event.type == sf::Event::Resized)
-                ; // TODO: Give the new size to the screen.
+                screen.resize(event.size.width, event.size.height);
         }
 
         // Update the widgets.
         auto elapsed_time = clock.restart();
-        container.update(elapsed_time);
+        screen.update(elapsed_time);
 
         // Draw the widgets.
         window.clear();
-        container.render(window);
+        screen.render(window);
         window.display();
     }
 }
