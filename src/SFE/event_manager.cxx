03c359de0646820cc1f29c674a580edc5fb79451
Philip Schill <philip.schill@gmx.de>
2017-06-11 18:20:22 +0200

    The sfe::EventManager class is now implemented with the pimpl idiom.

--- src/SFE/event_manager.cxx
+++ src/SFE/event_manager.cxx
@@ -1,33 +1,37 @@
 #include <SFE/event_manager.hxx>
 
 #include <algorithm>
+#include <map>
+#include <queue>
+#include <set>
+#include <vector>
 
 namespace sfe
 {
 
     Event::Event(std::string const& name)
         :
-        name_(name)
+        name_{ name }
     {}
 
-    bool Event::operator<(Event const & other) const
+    bool Event::operator<(Event const& other) const
     {
         return name_ < other.name_;
     }
 
-    bool Event::operator==(Event const & other) const
+    bool Event::operator==(Event const& other) const
     {
         return name_ == other.name_;
     }
 
-    Listener::Listener(Callback f)
+    Listener::Listener(Callback const& f)
         :
-        callback_(std::move(f))
+        callback_{ f }
     {}
 
-    void Listener::set_callback(Callback f)
+    void Listener::set_callback(Callback const& f)
     {
-        callback_ = std::move(f);
+        callback_ = f;
     }
 
     void Listener::notify(Event const& event) const
@@ -36,14 +40,77 @@ namespace sfe
             callback_(event);
     }
 
-    EventManager::EventManager() = default;
+    class EventManager::impl
+    {
+    public:
+
+        std::shared_ptr<Listener> register_listener(
+            Event const& event,
+            Listener::Callback const& callback
+        );
+
+        void enqueue(Event const& event);
+
+        void dispatch();
+
+        void register_event(Event const& event);
+
+    private:
+
+        ////////////////////////////////////////////////////////////
+        /// The registered listeners.
+        ////////////////////////////////////////////////////////////
+        std::map<Event, std::vector<std::weak_ptr<Listener>>> listeners_;
+
+        ////////////////////////////////////////////////////////////
+        /// The enqueued events.
+        ////////////////////////////////////////////////////////////
+        std::queue<Event> event_queue_;
+
+        ////////////////////////////////////////////////////////////
+        /// The list with the registered events.
+        ////////////////////////////////////////////////////////////
+        std::set<Event> registered_events_;
+
+    };
+
+    EventManager::EventManager()
+        :
+        impl_{ std::make_unique<impl>() }
+    {}
+
+    EventManager::~EventManager() = default;
 
     EventManager::EventManager(EventManager && other) = default;
 
     EventManager& EventManager::operator=(EventManager && other) = default;
+    
+    std::shared_ptr<Listener> EventManager::register_listener(
+        Event const & event,
+        Listener::Callback const& callback
+    ){
+        return impl_->register_listener(event, callback);
+    }
 
-    std::shared_ptr<Listener> EventManager::register_listener(Event const & event, Listener::Callback callback)
+    void EventManager::enqueue(Event const& event)
     {
+        impl_->enqueue(event);
+    }
+
+    void EventManager::dispatch()
+    {
+        impl_->dispatch();
+    }
+
+    void EventManager::register_event(Event const& event)
+    {
+        impl_->register_event(event);
+    }
+
+    std::shared_ptr<Listener> EventManager::impl::register_listener(
+        Event const& event,
+        Listener::Callback const& callback
+    ){
 #ifdef CHECKEVENTTYPE
         if (registered_events_.count(event) == 0)
             throw EventException("EventManager::register_listener(): Tried to register a listener for an unregistered event.");
@@ -53,12 +120,7 @@ namespace sfe
         return listener;
     }
 
-    void EventManager::enqueue(Event const & event)
-    {
-        enqueue(Event(event));
-    }
-
-    void EventManager::enqueue(Event && event)
+    void EventManager::impl::enqueue(Event const& event)
     {
 #ifdef CHECKEVENTTYPE
         if (registered_events_.count(event) == 0)
@@ -67,7 +129,7 @@ namespace sfe
         event_queue_.push(std::move(event));
     }
 
-    void EventManager::dispatch()
+    void EventManager::impl::dispatch()
     {
         // Erase all listeners that were destroyed.
         for (auto & event_listeners : listeners_)
@@ -83,9 +145,10 @@ namespace sfe
             listener_vector.erase(it, listener_vector.end());
         }
 
+        // Notify the listeners on the events
         while (!event_queue_.empty())
         {
-            auto const & ev = event_queue_.front();
+            auto const& ev = event_queue_.front();
 
             // Iterate over a copy because the callbacks may add new listeners,
             // and we do not want to change a container while iterating.
@@ -101,7 +164,7 @@ namespace sfe
         }
     }
 
-    void EventManager::register_event(Event const & event)
+    void EventManager::impl::register_event(Event const& event)
     {
         registered_events_.insert(event);
     }
